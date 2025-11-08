d67ca165a74189f73f5f98eff506863430d75b60
Ben Benyamin <benbin52+git@gmail.com>
2025-11-07 19:16:58 -0600

    Added NO-OP penalty, stoch action delay

--- fall/inertia_warpper.py
+++ fall/inertia_warpper.py
@@ -15,6 +15,82 @@ from utils import alpha_blit_rgb , load_icon_and_resize , convert_obs_to_graysca
 from utils import UP, DOWN, NO_OP
 from utils import ALE_EFFECTIVE_ACTION_MAP
 from gymnasium import spaces
+import pickle
+
+
+class PongDelayStochasticInertiaWrapper(gym.Wrapper):
+    """
+    Adds inertia behavior to discrete actions (NO_OP, UP, DOWN) by inserting
+    internal steps with previous/NO_OP actions before executing the requested
+    action.
+    """
+
+    def __init__(self, env, delay_steps: int = 10,lat_pickle_filename:str = "",target_fps:int = 30):
+        super().__init__(env)
+        self._delay_steps = int(delay_steps)
+        self.prev_action = NO_OP
+
+        with open(lat_pickle_filename, "rb") as f:
+            lat = pickle.load(f)
+        
+        self.target_fps = target_fps
+        
+        self.latencies = (target_fps*np.array(lat)/1000).astype(int)
+
+        self.sigma = np.std(self.latencies, ddof=1)
+
+
+    def reset(self, **kwargs):
+        obs, info = self.env.reset(**kwargs)  # Gymnasium API
+        self.prev_action = NO_OP
+        return obs, info
+
+    @property
+    def delay_steps(self):
+
+        lat = np.random.choice(self.latencies) + np.random.normal(0, self.sigma)
+        return int(lat)
+
+
+    def _run_steps(self, action: int, n_steps: int, total_reward: float):
+        """
+        Advance n_steps internally, accumulating rewards, and break if episode ends.
+        Returns: obs, total_reward, terminated, truncated, info, done_flag
+        """
+        obs, reward, terminated, truncated, info = None, 0.0, False, False, {}
+        for _ in range(n_steps):
+            obs, reward, terminated, truncated, info = self.env.step(action)
+            total_reward += float(reward)
+            if terminated or truncated:
+                return obs, total_reward, terminated, truncated, info, True
+        return obs, total_reward, terminated, truncated, info, False
+
+    def step(self, action: int):
+        total_reward = 0.0
+        if self.prev_action != action:
+            if action == NO_OP:
+                # Coasting inertia: half delay on previous action
+                obs, total_reward, terminated, truncated, info, done = \
+                    self._run_steps(self.prev_action, self.delay_steps // 2, total_reward)
+                if done:
+                    return obs, total_reward, terminated, truncated, info
+            else:
+                # First half: previous action
+                obs, total_reward, terminated, truncated, info, done = \
+                    self._run_steps(self.prev_action, self.delay_steps // 2, total_reward)
+                if done:
+                    return obs, total_reward, terminated, truncated, info
+                # Second half: NO_OP
+                obs, total_reward, terminated, truncated, info, done = \
+                    self._run_steps(NO_OP, self.delay_steps // 2, total_reward)
+                if done:
+                    return obs, total_reward, terminated, truncated, info
+
+        # Finally, execute desired action
+        self.prev_action = action
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        total = float(reward) + total_reward
+        return obs, total, terminated, truncated, info
 
 class PongDelayInertiaWrapper(gym.Wrapper):
     """
@@ -48,7 +124,6 @@ class PongDelayInertiaWrapper(gym.Wrapper):
 
     def step(self, action: int):
         total_reward = 0.0
-        # action = ALE_EFFECTIVE_ACTION_MAP[action]
         if self.prev_action != action:
             if action == NO_OP:
                 # Coasting inertia: half delay on previous action
@@ -74,6 +149,7 @@ class PongDelayInertiaWrapper(gym.Wrapper):
         total = float(reward) + total_reward
         return obs, total, terminated, truncated, info
 
+
 class ActionSubsetWrapper(gym.ActionWrapper):
     """
     Restrict the environment's discrete actions to a chosen subset.
@@ -89,6 +165,25 @@ class ActionSubsetWrapper(gym.ActionWrapper):
     def action(self, act):
         # Map reduced index to actual environment action
         return self.allowed_actions[act]
+
+class ActionPenalty(gym.Wrapper):
+
+    def __init__(self, env , penalized_actions:list, penalty:int = 0.2):
+        
+        super().__init__(env)
+        self.penalty = penalty
+        self.penalized_actions = penalized_actions
+    
+    def step(self, action):
+
+        obs, reward, terminated, truncated, info = self.env.step(action)
+
+        if action in self.penalized_actions:
+
+            reward -= self.penalty
+        
+        return obs, reward, terminated, truncated, info
+
 
 def make_wrapper_chain(wrappers):
     class WrapperChain(gym.Wrapper):
--- fall/train.py
+++ fall/train.py
@@ -3,59 +3,48 @@ import multiprocessing as mp
 mp.set_start_method('spawn', force=True)
 
 import gymnasium as gym
-from stable_baselines3 import PPO , DQN
-from sb3_contrib import RecurrentPPO
+from stable_baselines3 import PPO , DQN ,A2C
+from sb3_contrib import RecurrentPPO  ,QRDQN, TRPO,MaskablePPO
 
-from inertia_warpper import  PongDelayInertiaWrapper , make_wrapper_chain , ActionSubsetWrapper
-# from make_env import make_pong_env
-from functools import partial
+from inertia_warpper import  PongDelayInertiaWrapper , ActionSubsetWrapper,ActionPenalty
+from inertia_warpper import make_wrapper_chain
 
-from gymnasium.wrappers.atari_preprocessing import AtariPreprocessing
 from stable_baselines3.common.atari_wrappers import AtariWrapper
-from stable_baselines3.common.vec_env import SubprocVecEnv
-from stable_baselines3.common.vec_env import VecTransposeImage
-from stable_baselines3.common.monitor import Monitor
+
 from stable_baselines3.common.env_util import make_atari_env , make_vec_env
 from stable_baselines3.common.callbacks import CallbackList, CheckpointCallback
 
-from utils import ALE_ACTION_MAP
+from utils import ALE_ACTION_MAP ,REV_ACTION_MAP
 
 import ale_py
 gym.register_envs(ale_py)
 
 
-POLICY = RecurrentPPO
+POLICY = DQN
 
 ENV_NAME = "PongNoFrameskip-v4"
 TOTAL_TIMESTEPS = int(100e6)
 BATCH_SIZE = 64
-DELAY = 0
+DELAY = 18
 REW = 0.0
 MODEL_PATH = f"{POLICY.__name__}_delay{DELAY}_reward={REW}"
 N_ENVS = 8
 
-def make_pong_env():
-
-  env = gym.make(ENV_NAME)
-  env = AtariWrapper(env)
-  env = PongDelayInertiaWrapper(env,delay_steps=DELAY)
-
-  return env
-
 if __name__ == "__main__":
 
   wrap = make_wrapper_chain([
-    (AtariWrapper, {}),
     (PongDelayInertiaWrapper,{"delay_steps": DELAY}),
+    (AtariWrapper, {}),
     (ActionSubsetWrapper, {"allowed_actions": 
                            [k for k, v in ALE_ACTION_MAP.items() if v in {"NO-OP", "RIGHT", "LEFT"}]}),
+    # (ActionPenalty,{"penalized_actions": [REV_ACTION_MAP["NO-OP"]],"penalty":REW}),
   ])
 
   env = make_vec_env(ENV_NAME,N_ENVS,wrapper_class=wrap)
   
   obs = env.reset()
 
-  model = POLICY("CnnLstmPolicy", 
+  model = POLICY("CnnPolicy", 
                  env, 
                  verbose=1 , 
                  device="cuda",
@@ -64,17 +53,18 @@ if __name__ == "__main__":
                  )
 
 
-  # checkpoint_callback = CheckpointCallback(
-  #   save_freq=10_000,
-  #   save_path="./EXT_DATA/Pong-checkpoints/",
-  #   name_prefix=f"{POLICY.__name__}_Delay={DELAY}_REW={REW}",
-  #   save_replay_buffer=True,
-  #   save_vecnormalize=True,
-  # )
+  checkpoint_callback = CheckpointCallback(
+    save_freq=10_000,
+    save_path="./EXT_DATA/Pong-checkpoints/",
+    name_prefix=f"{POLICY.__name__}_Delay={DELAY}_REW={REW}",
+    save_replay_buffer=False,
+    save_vecnormalize=True,
+  )
 
   model.learn(
               TOTAL_TIMESTEPS, 
-              tb_log_name= f"{POLICY.__name__}_Delay={DELAY}_REW={REW}" , 
+              tb_log_name= f"{POLICY.__name__}_Delay={DELAY}_REW={REW}" ,
+              callback= checkpoint_callback,
               )
 
   model.save(MODEL_PATH)
